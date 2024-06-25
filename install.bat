e815f71e6392a0e2a96e312ec475bbc39add36d9
jonfreibr <jfreivald@brmedical.com>
2024-06-25 07:26:08 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,14 @@
 @echo off
+rem Example install batch file for Windows environment.
+rem Run from the application directory
+rem Required files:
+rem     logo.png                    logo file displayed by application at runtime
+rem     NPI Extraction.lnk          create this shortcut file to reflect your installation and runtime requirements!!
+rem     npi.py                      The main application file
+rem     python-3.11.5-amd64.exe     The Python source file
+rem     requirements.txt            Used by Python to install required libraries
+rem     unattend.xml                Required to run a "hands-off" install of Python.
+rem
 echo Installing Python 3.11.5
 python-3.11.5-amd64.exe /passive
 echo upgrading pip
